cf41651ab4e79b08a87b6980acad7b0f0648ff91
Avinash Kumar <avinashmadhukar4@gmail.com>
2025-04-02 16:34:38 +0530

    update in herBanner Talk About project

--- src/Components/HeroBanner/HeroBanner.jsx
+++ src/Components/HeroBanner/HeroBanner.jsx
@@ -25,7 +25,7 @@ const HeroBanner = () => {
                   of assets, patient care, patient health data, etc., which
                   helps to offer effective treatment.
                 </p>
-                <a href="portfolio" className="btn-white mt-4 fw-bold">
+                <a href="https://indtech-portfolio-page.vercel.app/" className="btn-white mt-4 fw-bold">
                   <span>Talk about Your Project</span>
                   <i className="icon icon-arrow"></i>
                 </a>
